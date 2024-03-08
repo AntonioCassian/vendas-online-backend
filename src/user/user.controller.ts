ffff7c818835b65ffdf67ffb1186426fcdcd2e77
AntonioCassian <antoniomart849@gmail.com>
2024-03-08 09:05:32 -0300

    feat: create post

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create.user.dto';
 
 @Controller('user')
 export class UserController {
-  @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  @Post()
+  async CreateUser(
+    @Body() createUser: CreateUserDto
+  ) {
+    return {
+      ...createUser,
+      password: undefined
+    }
   }
 }
+
+
+// dtos: são informações transferidas
+//interface: são os dados do úsuario em si
