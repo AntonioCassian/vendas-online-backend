57b5b2e5240bf3948ea4700a04e612ce71f92abe
AntonioCassian <antoniomart849@gmail.com>
2024-03-12 15:48:13 -0300

    feat: create paste  city

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '../cache/cache.module';
+import { CityService } from './city.service';
+import { CityEntity } from './entityes/city.entity';
+import { CityController } from './city.controller';
 
-@Module({})
+@Module({
+  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity])],
+  controllers: [CityController],
+  providers: [CityService],
+  exports: [CityService],
+})
 export class CityModule {}
